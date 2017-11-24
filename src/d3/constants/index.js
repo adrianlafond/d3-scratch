ee04d14ba027616ba0ee719a1c18685c8e9ec403
Adrian Lafond <mail@adrianlafond.com>
2017-11-23 22:36:50 -0500

    Updates

--- src/d3/constants/index.js
+++
@@ -1,1 +0,0 @@
-export const CONTAINER = '.d3-scratchpad'
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './index.css'
-import App from './components/app'
-import pages from './pages.json'
-
-const app = new App(pages)
-
-export default { app }
